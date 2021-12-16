d2e29b7133e798d3363e44473ddf5bf0d161e104
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2021-12-16 21:02:12 +0100

    Improves day 6 solution by using a non recursive function

--- ed2021/src/bin/p6.rs
+++ ed2021/src/bin/p6.rs
@@ -1,37 +1,20 @@
 use std::str::FromStr;
 
-macro_rules! childs {
-    () => { sum };
-    ($_:tt $($t:tt)*) => { |i| (1..=i).map(childs!($($t)*)).sum::<usize>() };
-}
-
 const INPUT: &str = include_str!("../../assets/p6.in");
 
-const fn sum(val: usize) -> usize {
-    val * (val + 1) / 2
-}
-
 fn childs_of_gen<const DAYS: usize>(gen: usize, start: usize) -> usize {
-    fn inner(gen: usize, childs: usize) -> usize {
-        match gen {
-            0 => 1,
-            // redundant: speeds up
-            1 => childs,
-            2 => sum(childs),
-            3 => childs!(?)(childs),
-            4 => childs!(??)(childs),
-            5 => childs!(???)(childs),
-            6 => childs!(????)(childs),
-            7 => childs!(?????)(childs),
-            8 => childs!(??????)(childs),
-            _ => (1..=childs).map(|i| inner(gen - 1, i)).sum(),
-        }
+    /// Number of childs made by the genaration x if the first child of the
+    /// previous generation made y children
+    fn inner(x: u128, y: u128) -> u128 {
+        (0..x).map(|v| v + y).product::<u128>() / (1..=x).product::<u128>()
     }
     let prev = gen.saturating_sub(1);
+    // number of children made by the first child of the previous generation
     let childs = (DAYS + 6).saturating_sub(start + prev * 9) / 7;
-    inner(gen, childs)
+    usize::try_from(inner(gen as u128, childs as u128)).unwrap()
 }
 
+/// Total number of children made in DAYS days by starting from the state start
 fn childs<const DAYS: usize>(start: usize) -> usize {
     (0..)
         .map(|gen| childs_of_gen::<DAYS>(gen, start))
@@ -39,6 +22,7 @@ fn childs<const DAYS: usize>(start: usize) -> usize {
         .sum()
 }
 
+/// Precompute the number of childern made in DAYS days for every starting state
 fn precompute<const DAYS: usize>() -> [usize; 7] {
     [0, 1, 2, 3, 4, 5, 6].map(childs::<DAYS>)
 }
