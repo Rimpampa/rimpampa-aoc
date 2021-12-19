9bdf470713787c7ad4c4d244b60de9f1927b9491
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2021-12-19 13:08:14 +0100

    Removes test inputs

--- ed2021/src/bin/p4.rs
+++ ed2021/src/bin/p4.rs
@@ -63,7 +63,7 @@ impl<'a> Board<'a> {
 
 fn solve<const FIRST_OR_LAST: bool>(input: &str) -> usize {
     let (first, rest) = input.split_once("\n\n").unwrap();
-    let numbers = first.split(",");
+    let numbers = first.split(',');
     let boards = rest.split("\n\n").map(Board::new);
     let win_boards = boards
         .clone()
@@ -99,7 +99,26 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    const TEST_INPUT: &str = include_str!("../../assets/p4-test.in");
+    const TEST_INPUT: &str = "\
+        7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\
+        \n\
+        22 13 17 11  0\n\
+         8  2 23  4 24\n\
+        21  9 14 16  7\n\
+         6 10  3 18  5\n\
+         1 12 20 15 19\n\
+        \n\
+         3 15  0  2 22\n\
+         9 18 13 17  5\n\
+        19  8  7 25 23\n\
+        20 11 10 24  4\n\
+        14 21 16 12  6\n\
+        \n\
+        14 21 17 24  4\n\
+        10 16 15  9 19\n\
+        18  8 23 26 20\n\
+        22 11 13  6  5\n\
+         2  0 12  3  7\n";
 
     #[test]
     fn test() {
